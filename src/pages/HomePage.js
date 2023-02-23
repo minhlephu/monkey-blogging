951a8368cfba05d2608b89f0d5321f0d1f4c75be
minhlephu <110329077+minhlephu@users.noreply.github.com>
2023-02-23 17:51:31 +0700

    giao dien Header

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,14 @@
+import Header from "components/layout/Header";
 import { auth } from "firebase-app/firebase-config";
 import { signOut } from "firebase/auth";
 import React from "react";
-
+import styled from "styled-components";
+const HomePageStyleds = styled.div``;
 const HomePage = () => {
-  const handleSignOut = () => {
-    signOut(auth);
-  };
   return (
-    <div>
-      <button onClick={handleSignOut}>SignOut</button>
-    </div>
+    <HomePageStyleds>
+      <Header></Header>
+    </HomePageStyleds>
   );
 };
 
